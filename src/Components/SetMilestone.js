24ae555aaf0135672e56bb9b773e67e5ce981908
Ajitesh13 <aji.yash13@gmail.com>
2020-10-14 13:54:47 +0530

    progress bar  and pie chart done

--- src/Components/SetMilestone.js
+++ src/Components/SetMilestone.js
@@ -12,39 +12,7 @@ const setmilestones = () => {
     const projectname = "Select Project";
 
     const updateprojectstatus = async () => {
-        try {
-            const res = await fetch('http://localhost:5000/projects/5f86012389b2c90ed4823260');
-            const data = await res.json();
-            console.log(data.length);
-            for (var i = 0; i < data.length; i++) {
-                if (data[i]._id === Cookies.get('projectid')) {
-                    for (var j = 0; j < data[i].milestones.length; j++) {
-                        if (data[i].milestones[j].title === Cookies.get('completedMilestone')) {
-                            const updatedata = {
-                                "projects": [{
-                                    "milestones": [
-                                        {
-                                            "title": Cookies.get('completedMilestone'),
-                                            "status": "true",
-                                            "_id": data[i].milestones[j]._id,
-                                        }
-                                    ]
-                                }]
-                            };
-                            axios.post(`http://localhost:5000/addProject/5f82c21d03d9de36665a08ec`,updatedata )
-                            .then (res=>{
-                                console.log(res.data)
-                            });
-                            return;
-                        }
-                    }
-                }
-            }
-            
-
-        } catch (err) {
-            console.error(err);
-        }
+        // code to update status should be added here
     }
 
     function addsession() {
@@ -52,24 +20,24 @@ const setmilestones = () => {
         const duration = ((Cookies.get('endmin') - Cookies.get('startmin'))*60 +  Cookies.get('endsec') - Cookies.get('startsec'));
         const doneMilestone = Cookies.get('completedMilestone');
 
-        // const data = {
-        //     "projects": [{
-        //         "session": [
-        //             {
-        //                 "start_time":"",
-        //                 "stop_time":"",
-        //                 "duration": duration,
-        //                 "milestones_covered": [
-        //                     doneMilestone
-        //                 ]
-        //             }
-        //         ]
-        //     }]
-        // };
-        // axios.post(`http://localhost:5000/addSession/5f86012389b2c90ed4823260/${projectId}`,data )
-        // .then (res=>{
-        //     console.log(res.data)
-        // })
+        const data = {
+            "projects": [{
+                "session": [
+                    {
+                        "start_time":"",
+                        "stop_time":"",
+                        "duration": duration,
+                        "milestones_covered": [
+                            doneMilestone
+                        ]
+                    }
+                ]
+            }]
+        };
+        axios.post(`http://localhost:5000/addSession/5f86012389b2c90ed4823260/${projectId}`,data )
+        .then (res=>{
+            console.log(res.data)
+        })
 
         updateprojectstatus();
     }
